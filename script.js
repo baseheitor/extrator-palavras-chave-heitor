ea1400e8cef804d150e4dddac64f7ebcd8e51539
heitor base <heitor.base@escola.pr.gov.br>
2025-10-21 09:25:22 -0300

    21/10

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-
-}
 alert("Fui clicado")
 }
