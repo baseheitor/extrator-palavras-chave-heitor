7fb3250c05cc7a722bf592d305489c024f801548
heitor base <heitor.base@escola.pr.gov.br>
2025-10-28 09:32:34 -0300

    28/10

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-const texto = document.querySelector("#entrada-de-texto").value;
-const campoResultado = document.querySelector("#resultado-palavrachave");
-const palavras = processaTexto (texto);
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave = processaTexto(texto);
 
-campoResultado.textContent = mostraPalavrasChave.join(", ")
+    campoResultado.textContent = PalavrasChave.join(", ")
 }
 
-function processaTexto(){
-let palavras = texto.split(/\P{L}+?/u);
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
 
-return palavras
+    return palavras
 
 }
